6cec2c8009e689e4592ef2e5264cdb79a0524236
lijunfeng <junfeng.li@hand-china.com>
2021-12-06 20:58:52 +0800

    Splice & ComponentEmitsType

--- packages/ts-learn/practice/string/10_CamelCase.ts
+++ packages/ts-learn/practice/string/10_CamelCase.ts
@@ -4,7 +4,7 @@ export default {}
 // 一样的判断方案
 // 问题在于什么时候进行转化，可以写三个infer 当中间的这个是 - 的时候表示前面的需要进行大写
 
-type CamelCase<T, Pre extends string = '', F extends boolean = true> =
+export type CamelCase<T, Pre extends string = '', F extends boolean = true> =
   T extends `${infer R}${infer S}` // 当没有的时候表示走完了
   ? (
     // 如果R跟大写后的等价，那么就要转成小写，并且假如不是第一个，就要给前面的添加一个 -
--- packages/ts-learn/practice/tuple/13_Splice.ts
+++ packages/ts-learn/practice/tuple/13_Splice.ts
@@ -1,7 +1,25 @@
 
-// type A1 = Splice<[string, number, boolean, null, undefined, never], 0, 2>                   
-// // [boolean,null,undefined,never]               从第0开始删除，删除2个元素
-// type A2 = Splice<[string, number, boolean, null, undefined, never], 1, 3>                   
-// // [string,undefined,never]                     从第1开始删除，删除3个元素
-// type A3 = Splice<[string, number, boolean, null, undefined, never], 1, 2, [1, 2, 3]>        
-// // [string,1,2,3,null,undefined,never]          从第1开始删除，删除2个元素，替换为另外三个元素1,2,3
+// 应该跟Slice是一个反向的操作
+
+type Splice<
+  T extends any[], S extends number, E extends number = T['length'],
+  Replace extends any[] = [],
+  SA extends any[] = [], EA extends any[] = [], Pre extends any[] = []
+  > =
+  T extends [infer L, ...infer R]
+  ? SA['length'] extends S
+    ? EA['length'] extends E
+      // 对Replace的内容进行处理
+      ? Replace extends [infer RR, ...infer RL]
+        ? Splice<[L,...R], S, E, RL, SA, EA, [...Pre, RR]>
+        : Splice<R, S, E, Replace, SA, EA, [...Pre, L]>
+      : Splice<R, S, E, Replace, SA, [...EA, L], [...Pre]>
+    : Splice<R, S, E, Replace, [...SA, L], EA, [...Pre, L]>
+  : Pre
+
+type A1 = Splice<[string, number, boolean, null, undefined, never], 0, 2>
+// [boolean,null,undefined,never]               从第0开始删除，删除2个元素
+type A2 = Splice<[string, number, boolean, null, undefined, never], 1, 3>
+// [string,undefined,never]                     从第1开始删除，删除3个元素
+type A3 = Splice<[string, number, boolean, null, undefined, never], 1, 2, [1, 2, 3]>
+// [string,1,2,3,null,undefined,never]          从第1开始删除，删除2个元素，替换为另外三个元素1,2,3
